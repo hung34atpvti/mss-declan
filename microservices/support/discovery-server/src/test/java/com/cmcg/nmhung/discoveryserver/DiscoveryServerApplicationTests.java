02909f6b60144c2d88cecf5de2f0dce166813f87
nmhung@cmc.com.vn <nmhung@cmc.com.vn>
2021-08-10 21:11:19 +0700

    fixed mvn

--- microservices/support/discovery-server/src/test/java/com/cmcg/nmhung/discoveryserver/DiscoveryServerApplicationTests.java
+++ microservices/support/discovery-server/src/test/java/com/cmcg/nmhung/discoveryserver/DiscoveryServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.mss.declan.eureka;
+package com.cmcg.nmhung.discoveryserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaApplicationTests {
+class DiscoveryServerApplicationTests {
 
     @Test
     void contextLoads() {

54733261ddda061c431e32b87fb2bc5c2a1b333c
nmhung <nmhung@cmc.com.vn>
2021-09-18 09:51:58 +0700

    build file for docker

---
+++ build-docker.sh
@@ -0,0 +1,31 @@
+#BUILD SUPPORT
+#build discovery server
+cd microservices/support/discovery-server/
+mvn clean install -Dmaven.test.skip=true
+docker build -t eureka-server .
+#build gateway server
+cd ../edge-server/
+mvn clean install -Dmaven.test.skip=true
+docker build -t zuul-server .
+#build auth-server
+cd ../auth-server/
+mvn clean install -Dmaven.test.skip=true
+docker build -t auth-server .
+#BUILD CORE
+#build product-service
+cd ../../core/product-service/
+mvn clean install -Dmaven.test.skip=true
+docker build -t product-service .
+#build recommendation-service
+cd ../recommendation-service/
+mvn clean install -Dmaven.test.skip=true
+docker build -t recommendation-services .
+#build review-service
+cd ../review-service/
+mvn clean install -Dmaven.test.skip=true
+docker build -t review-service .
+#BUILD COMPOSITE
+#build product-composite-service
+cd ../../composite/product-composite-service/
+mvn clean install -Dmaven.test.skip=true
+docker build -t product-composite-service .

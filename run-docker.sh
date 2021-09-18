e68405da06a8e939261c201f9011f687be8705e1
nmhung <nmhung@cmc.com.vn>
2021-09-18 16:12:34 +0700

    elk stack

--- run-docker.sh
+++ run-docker.sh
@@ -1,4 +1,4 @@
 #docker clean
-docker rm -f eureka-server zuul-server auth-service product-service recommendation-service review-service product-composite-service
+docker rm -f eureka-server zuul-server auth-service product-service recommendation-service review-service product-composite-service elasticsearch logstash kibana
 #docker-compose run
 docker-compose up

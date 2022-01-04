7788d756c45d195bfbcc5e158faca1ef6f688206
declan <nmhung@cmc.com.vn>
2022-01-04 11:57:02 +0700

    fix run sh with elk

--- run-docker-mss-within-elk.sh
+++ run-docker-mss-within-elk.sh
@@ -1,4 +1,4 @@
 #docker clean
-docker rm -f consul consul-agent consul-server consul-server-bootstrap zuul-server auth-service product-service recommendation-service review-service product-composite-service elasticsearch logstash kibana
+docker rm -f zipkin rabbitmq consul consul-agent consul-server consul-server-bootstrap zuul-server auth-service product-service recommendation-service review-service product-composite-service es-container ls-container kb-container
 #docker-compose run
-docker-compose -f docker-compose-elk.yml -f docker-compose-mss-elk.yml up
+docker-compose -f docker-compose-elk.yml -f docker-compose-mss-within-elk.yml up
